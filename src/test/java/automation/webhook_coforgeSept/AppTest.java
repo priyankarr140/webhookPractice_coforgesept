c033ff802dfaa483166c067e1852852887f8940f
priyanka <priyankarr140@gmail.com>
2025-09-01 09:46:18 +0530

    initial commit

---
+++ src/test/java/automation/webhook_coforgeSept/AppTest.java
@@ -0,0 +1,16 @@
+package automation.webhook_coforgeSept;
+import org.testng.annotations.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+      System.out.println("***LOGIN");
+    }
+}
